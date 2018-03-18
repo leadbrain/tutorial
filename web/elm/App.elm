bae29d289f3670ed03ee1d48b1f8fe51e2504631
yskim <ys.kim@riiid.co>
2018-03-18 15:08:39 +0900

    module 분리

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,60 +1,18 @@
 module App exposing (..)
 
 import Html exposing (Html, div, text, program)
-
-
--- MODEL
-
-
-type alias Model =
-    String
-
+import Messages exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
 init : ( Model, Cmd Msg )
 init =
     ( "Hello", Cmd.none )
 
-
-
--- MESSAGES
-
-
-type Msg
-    = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-
--- MAIN
 
 
 main : Program Never Model Msg
